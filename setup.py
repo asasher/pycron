33206e8db8476bf1954e6ad357a8274ff34f53f1
Asher <muhammad.anjum@careem.com>
2019-05-26 16:04:15 +0400

    Update setup and test-requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='pycron',
-    version='1.0.0',
+    version='1.0.1',
     description='Simple cron-like parser, which determines if current datetime matches conditions.',
-    author='Kimmo Huoman',
-    author_email='kipenroskaposti@gmail.com',
-    url='https://github.com/kipe/pycron',
+    author='Asher',
+    author_email='asher_as@live.com',
+    url='https://github.com/asasher/pycron',
     packages=[
         'pycron',
     ],
